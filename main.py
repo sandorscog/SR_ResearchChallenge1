6ecaa53288b83316e18c62e65cc669112d0f70d5
Sandor <sandorscog@duck.com>
2023-09-24 21:07:14 -0300

    Media por item

--- main.py
+++ main.py
@@ -23,6 +23,20 @@ def users_means(ratings):
     return scores
 
 
+def items_means(ratings):
+    scores = {key: [] for key in ratings['ItemId'].unique().tolist()}
+
+    for row in ratings.itertuples():
+        scores[row.ItemId].append(row.Rating)
+
+    for key in scores.keys():
+        values = scores[key]
+        mean = np.sum(values)/len(values)
+        scores[key] = mean
+
+    return scores
+
+
 def dense_representation_user(data, mean):
 
     dense_representation = {key: [] for key in data['UserId'].unique().tolist()}
@@ -53,9 +67,26 @@ def cosine(u, v):
     return cos
 
 
+def neighborhood_viability(object_index):
+
+    not_viable = []
+    for obj in object_index.keys():
+        raters = object_index[obj]
+        if len(raters) < 2:
+            not_viable.append(obj)
+
+    return not_viable
+
+
 def neighbors(object_index, user_index, target_items):
 
     neighborhoods = {key: [] for key in target_items}
+
+    for item in neighborhood_viability(object_index):
+        #print(item)
+        if item in target_items:
+            target_items.remove(item)
+            neighborhoods[item].append(('x', 0))
 
     tempting_objects = []
     for i in target_items:
@@ -69,7 +100,7 @@ def neighbors(object_index, user_index, target_items):
 
     # # # # # # # # # # # # # # #
     for i in target_items:
-        for j in tempting_objects[:150]:
+        for j in tempting_objects[:250]:
             u = []
             v = []
             if i == j:
@@ -91,23 +122,29 @@ def neighbors(object_index, user_index, target_items):
 
             neighborhoods[i].append((j, cos))
 
+    # print(neighborhoods)
     return neighborhoods
 
 
-def predict_ratings(targets, neighborhood, user_ratings, mean_values):
+def predict_ratings(targets, neighborhood, user_ratings, mean_values, items_mean_values):
 
     predictions = pd.DataFrame({'UserId:ItemId': [], 'Rating': []})
 
     for target in targets.itertuples():
-        #print(target.ItemId, target.UserId)
-        prediction = rating_prediction(target.ItemId, target.UserId, neighborhood, user_ratings)
-        prediction += mean_values[target.UserId]
 
         target_tuple = str(target.UserId) + ':' + str(target.ItemId)
-        try:
-            final_rating = int(round(prediction, 0))
-        except:
-            final_rating = randint(1, 5)
+
+        if neighborhood[target.ItemId][0][0] == 'x' or True:
+            final_rating = items_mean_values[target.ItemId]
+
+        else:
+            prediction = rating_prediction(target.ItemId, target.UserId, neighborhood, user_ratings)
+            prediction += mean_values[target.UserId]
+
+            try:
+                final_rating = int(round(prediction, 0))
+            except:
+                final_rating = randint(1, 5)
 
         prediction = pd.DataFrame({'UserId:ItemId': [target_tuple], 'Rating': [final_rating]})
         predictions = pd.concat([predictions, prediction])
@@ -120,6 +157,7 @@ def rating_prediction(item_t, user_t, neighborhood, user_ratings):
     user_t_ratings = dict(user_ratings[user_t])
 
     numerator = 0
+    # print(user_t_ratings)
     for neighbor in neighborhood[item_t]:
         if user_t_ratings.get(neighbor[0]) is not None and not np.nan:
             numerator += user_t_ratings[neighbor[0]]*neighbor[1]
@@ -146,18 +184,15 @@ def main():
     # Split association User:Item
     ratings[['UserId', 'ItemId']] = ratings['UserId:ItemId'].str.split(':', 1, expand=True)
     targets[['UserId', 'ItemId']] = targets['UserId:ItemId'].str.split(':', 1, expand=True)
-    # targets = targets[:100]
 
-    #print(len(targets['ItemId'].unique().tolist()))
-
-    #print('Representations')
     mean_values = users_means(ratings)
+    item_means = items_means(ratings)
     item_dense_representation = dense_representation_builder(ratings, mean_values)
     user_dense_representation = dense_representation_user(ratings, mean_values)
     #print('Neighbors')
     neighborhood = neighbors(item_dense_representation, user_dense_representation, targets['ItemId'].unique().tolist())
     #print('Predictions')
-    predictions = predict_ratings(targets, neighborhood, user_dense_representation, mean_values)
+    predictions = predict_ratings(targets, neighborhood, user_dense_representation, mean_values, item_means)
 
     predictions = predictions.astype({'Rating': int})
     predictions.to_csv(PATH+'resultado.csv', index=False)
@@ -168,14 +203,16 @@ def main():
 def teste():
 
     ratings = pd.read_csv(PATH+'ratings_teste.csv')
-    targets = pd.DataFrame({'UserId': ['A', 'B'], 'ItemId': ['banana', 'laranja']})
+    targets = pd.DataFrame({'UserId': ['A', 'B', 'A'], 'ItemId': ['banana', 'laranja', 'tomate']})
 
     mean_values = users_means(ratings)
+    item_means = items_means(ratings)
+
     dense_representation = dense_representation_builder(ratings, mean_values)
     user_dense_representation = dense_representation_user(ratings, mean_values)
 
     neighborhood = neighbors(dense_representation, user_dense_representation, targets['ItemId'].unique().tolist())
-    predictions = predict_ratings(targets, neighborhood, user_dense_representation, mean_values)
+    predictions = predict_ratings(targets, neighborhood, user_dense_representation, mean_values, item_means)
     predictions = predictions.astype({'Rating': int})
 
     predictions.to_csv(PATH + 'resultado.csv', index=False)
